ec045eef2fac7b8fd0b0e7586be6dccee348ebda
Elena_Zhelezova <Elena_Zhelezova@epam.com>
2021-04-19 20:35:07 +0300

    jenkinsfile added

---
+++ jenkinsfile.groovy
@@ -0,0 +1,45 @@
+pipeline {
+  environment {
+    registry = "elenazhelezova/exam_devops"
+    registryCredential = 'docker_id'
+    dockerImage = 'web_app'
+  }
+  agent any
+  stages {
+    stage('Cloning git repo'){
+      steps{
+        git
+        checkout([
+                $class: 'GitSCM',
+                branches: [[ name: "master" ]],
+                userRemoteConfigs: [[credentialsId: "git_id", url: "https://github.com/ElenaZhelezova/student-exam2.git"]]
+        ])
+      }
+    }
+    stage('Building image') {
+      steps{
+        script {
+          dockerImage = docker.build registry + ":$BUILD_NUMBER"
+        }
+      }
+    stage('Testing app') {
+      steps{
+        sh 'coverage run -m pytest'
+        sh 'coverage report'
+      }
+    stage('Deploy Image') {
+      steps{
+        script {
+          docker.withRegistry( '', registryCredential ) {
+            dockerImage.push()
+          }
+        }
+      }
+    }
+    stage('Remove Unused docker image') {
+      steps{
+        sh "docker rmi $registry:$BUILD_NUMBER"
+      }
+    }
+  }
+}
